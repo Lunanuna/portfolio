22ceab862162fecceaded829a2d11f3290b6e150
Gayoung-Han <104375515+Lunanuna@users.noreply.github.com>
2026-08-17 11:00:10 +0200

    added blasol link for now
    daaaamn

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom';
+
 export default function Projects(){
     return(
         <>
         <section>
             <h2>Projects</h2>
-            <div>1. Blå Sol</div>
+            <h3>This will be sub detail pages this week i promise..</h3>
+            <div>1. Blå Sol <a href="https://www.figma.com/proto/lc6FkxRIVe9zgSVkhhXeCU/Portfolio-Project?node-id=1-220&p=f&viewport=225%2C-465%2C0.56&t=9IzbhjCxYPSigh12-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1">click here</a></div>
             <div>2. Spil Cafeen</div>
             <div>3. CPH FW</div>
         </section>
