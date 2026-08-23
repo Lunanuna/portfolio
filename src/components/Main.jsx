6f284448185402d87707673453c447358c5c67bc
Gayoung-Han <104375515+Lunanuna@users.noreply.github.com>
2026-08-23 13:48:28 +0200

    media quary and css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,29 +1,32 @@
 import styles from './Main.module.css';
 import profile from '../assets/images/profile/about.jpeg';
+import { Link } from 'react-router-dom';
 
 export default function Main() {
   return (
     <main className={styles.main}>
       <section className={styles.topSection}>
         <div className={styles.heading}>
-          <div className={styles.name}>My name is Gayoung</div>
           <div className={styles.role}>
-            <span>&</span> I am a UX/UI designer
+            UX/UI Designer
           </div>
+           <div className={styles.name}>Gayoung Han</div>
         </div>
-        <div className={styles.contact}>hanga93@gmail.com</div>
       </section>
 
       <section className={styles.bottomSection}>
         <img className={styles.profileImage} src={profile} alt="profile" />
         <div className={styles.introduction}>
-         <div>
-          Hi there! I am a Multimedia Design student specializing in UI/UX.
+         <div className={styles.introText}>
+          <p>Hejsa!</p>
+          <p>My name is Gayoung and I am studying Multimedia Design specializing in UI/UX.</p>
+          <p> I'm a detail-oriented and curious designer who likes to dig deeper into the things I notice.</p>
+          <p>I’m currently looking for <span className={styles.highlight}>an internship opportunity for Spring 2027 based in Copenhagen</span>, where I can learn, contribute, and grow as a designer.</p>
+          
          </div>
-         <div>
-          I’m currently looking for an internship opportunity for Spring 2027 based in Copenhagen, where I can learn, contribute, and grow as a designer.
-         </div>
-         
+         <nav>
+          <Link className={styles.navLink} to="/#Projects" >Explore My Projects</Link>
+         </nav>
           </div>
       </section>
     </main>
